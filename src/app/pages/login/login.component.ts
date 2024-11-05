90b0a9a79684b98a64e51c6eb7243fa0130ec027
machadoluan <machado.luandealmeida@gmail.com>
2024-11-04 21:28:58 -0300

    feat: add login com google

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input'
+import { GoogleSigninButtonModule, SocialAuthService, SocialLoginModule } from '@abacritt/angularx-social-login';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, GoogleSigninButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
+export class LoginComponent  {
   hidePassword = true;
+
 
 }

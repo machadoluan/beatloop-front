46613a847c129722674311e08fd48d0c129e25ac
machadoluan <machado.luandealmeida@gmail.com>
2024-11-06 20:51:38 -0300

    feat: add styles/requisição backend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -14,22 +15,48 @@ export class LoginComponent {
 
   showPassword = false;
   loginSocial = true;
+  animationClass: string = '';
+  userLoginForm: FormGroup;
 
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.userLoginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
 
-  toggleSocial(){
-    this.loginSocial = !this.loginSocial
+  toggleSocial() {
+    if (this.loginSocial) {
+      this.animationClass = 'fade-out';
+      setTimeout(() => {
+        this.loginSocial = !this.loginSocial;
+        this.animationClass = 'fade-in';
+      }, 500); // Tempo da animação em ms
+    } else {
+      this.animationClass = 'fade-out';
+      setTimeout(() => {
+        this.loginSocial = !this.loginSocial;
+        this.animationClass = 'fade-in';
+      }, 500);
+    }
   }
 
   login() {
     this.authService.login();
+  }
+
+  userLogin() {
+    this.authService.userLogin(this.userLoginForm.value).subscribe((response: any) => {
+      console.log(response);
+    });
   }
 }

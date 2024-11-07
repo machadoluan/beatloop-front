f8dfa91522d3bf4e2169e0ac7027bfbbb3e951ef
machadoluan <machado.luandealmeida@gmail.com>
2024-11-07 14:34:04 -0300

    feat: add modal de verificar codigo email

---
+++ src/app/components/verficar-email/verficar-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verficar-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './verficar-email.component.html',
+  styleUrl: './verficar-email.component.scss'
+})
+export class VerficarEmailComponent {
+
+}

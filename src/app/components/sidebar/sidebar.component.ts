b673ce7050b02b55a1cae017ead2df53dad20b59
machadoluan <machado.luandealmeida@gmail.com>
2024-11-11 15:46:08 -0300

    remove: removendo components de style do slider bar / add new design

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @Output() sidebarWidthChange = new EventEmitter<string>();
+
+  sidebarWidth: string = '230px';
+
+
+  constructor(private router: Router) { }
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 
 }

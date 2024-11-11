54e50a961d14ec5be2af041785c12ebff22a721f
machadoluan <machado.luandealmeida@gmail.com>
2024-11-11 15:44:58 -0300

    feat: add new rota para pagina de "packs"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthCallbackComponent } from './auth-callback.component';
 import { BrowseComponent } from './pages/browse/browse.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SamplesPacksComponent } from './pages/samples-packs/samples-packs.component';
 
 export const routes: Routes = [
   {
@@ -27,7 +29,15 @@ export const routes: Routes = [
     component: BrowseComponent
   },
   {
+    path: 'packs',
+    component: SamplesPacksComponent
+  },
+  {
     path: 'signup',
     component: CadastroComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];

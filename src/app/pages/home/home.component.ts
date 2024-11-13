3e3686dd6e034be59320319a7c8ba151c0b76cfe
machadolucasalmeida <machado.lucasdealmeida@gmail.com>
2024-11-12 23:05:38 -0300

    delete: removendo imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { HeaderComponent } from '../../components/header/header.component';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',

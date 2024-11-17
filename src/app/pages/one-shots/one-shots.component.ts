1d9db22745c7cdeb955058ee82b5de39de788bb6
machadolucasalmeida <machado.lucasdealmeida@gmail.com>
2024-11-16 21:25:35 -0300

    feat: add page one-shots

---
+++ src/app/pages/one-shots/one-shots.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-shots',
+  standalone: true,
+  imports: [],
+  templateUrl: './one-shots.component.html',
+  styleUrl: './one-shots.component.scss'
+})
+export class OneShotsComponent {
+
+}

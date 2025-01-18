ca8ac5f29f29905457fe31dfff9a6c9cc342b5c1
machadoluan <machado.luandealmeida@gmail.com>
2025-01-18 00:07:18 -0300

    feat: add fução de abrir e fechar slidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { SidebarService } from '../../services/sidebar.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -16,17 +17,24 @@ export class SidebarComponent {
   sidebarWidth: string = '80px';
   descButton: boolean = false;
   iconSlide: boolean = false;
+  isOpen$: Observable<boolean>;
 
   @Output() sidebarWidthChange = new EventEmitter<string>();
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.isOpen$ = this.sidebarService.isOpen$;
+  }
 
   isActive(route: string): boolean {
     return this.router.url === route;
   }
 
+  get isDescVisible(): Observable<boolean> {
+    return this.isOpen$.pipe(map(isOpen => this.descButton || isOpen));
+  }
 
-  toggleSlideBar(){
+
+  toggleSlideBar() {
     this.sidebarWidth = this.sidebarWidth === "80px" ? "200px" : "80px"
     this.descButton = !this.descButton
     this.iconSlide = !this.iconSlide

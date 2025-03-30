b8f5503ffb42f2225bf66810f3e7bbdf41bc10e0
machadoluan <machado.luandealmeida@gmail.com>
2025-03-30 18:01:32 -0300

    rem: remove AuthCallback route from application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthCallbackComponent } from './auth-callback.component';
 import { BrowseComponent } from './pages/browse/browse.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SamplesPacksComponent } from './pages/samples-packs/samples-packs.component';
@@ -48,10 +47,6 @@ export const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
-  },
-  {
-    path: 'auth-callback',
-    component: AuthCallbackComponent
   },
   {
     path: '**',
